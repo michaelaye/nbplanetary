2c33ba52ff1b855dbcdebfaf05ba7c7a0f4e12e4
Michael Aye <kmichael.aye@gmail.com>
2022-09-08 16:16:33 +0200

    implement new approach with environment file creation

--- .github/workflows/create_env_file.py
+++ .github/workflows/create_env_file.py
@@ -7,7 +7,7 @@ cfg = config["DEFAULT"]
 requirements = cfg.get("requirements", "").split()
 
 with open("environment.yml", "w") as f:
-    f.write("name: testenv\n")
+    f.write("name: my-env\n")
     f.write("channels: conda-forge\n")
     f.write("dependencies:\n")
     f.writelines([f"  - {r}\n" for r in requirements])
