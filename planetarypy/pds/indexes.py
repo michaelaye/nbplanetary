ee78d6720d125496e7e428b4b3726175ee06be26
Michael Aye <kmichael.aye@gmail.com>
2023-06-26 16:43:15 +0200

    add check_update switch
    * also change filename determination to work without URL for CTX

--- planetarypy/pds/indexes.py
+++ planetarypy/pds/indexes.py
@@ -6,14 +6,17 @@ __all__ = ['logger', 'storage_root', 'dynamic_urls', 'Index']
 # %% ../../notebooks/api/02a_pds.indexes.ipynb 3
 import logging
 from datetime import datetime
-from pathlib import Path
 from urllib.parse import urlsplit, urlunsplit
 from urllib.request import URLError
 
-import pandas as pd
 import tomlkit as toml
 from dateutil import parser
 from dateutil.parser import ParserError
+from yarl import URL
+
+import pandas as pd
+from fastcore.basics import patch  # better monkeypatcher
+from fastcore.xtras import Path  # improved pathlib.Path
 from .. import utils
 from ..config import config
 from .ctx_index import CTXIndex
@@ -38,8 +41,11 @@ class Index:
         self,
         key: str,  # Nested (dotted) key, e.g. cassini.iss.ring_summary
         url: str = None,  # URL to index. If not given, will be read from config object.
+        check_update: bool = True,  # switch if dynamic URLS will be derived upon start
     ):
         self.key = self.parse_key(key)
+        self.check_update = check_update
+
         self.set_url(url)
         try:
             self.timestamp = parser.parse(config.get_value(self.key)["timestamp"])
@@ -53,26 +59,12 @@ class Index:
                 self.timestamp = None
         self._remote_timestamp = None
 
-    def parse_key(
-        self,
-        key: str,  # dotted key
-    ):
-        """Take care of different ways how the key could be structured.
-
-        This involves adding the sub-key `indexes` for the config file structure,
-        which is something the user of this class should not need to know.
-        """
-        tmp = key if key.startswith("missions") else "missions." + key
-        subs = tmp.split(".")
-        if subs[3] != "indexes":
-            subs.insert(3, "indexes")
-        return ".".join(subs)
-
     def set_url(self, url):  # URL to index.
         """Set URL from having it dynamically determined (for non-static index URLs)."""
         self.url = config.get_value(self.key)["url"] if url is None else url
-        if not self.url:  # empty ''
+        if not self.url and self.check_update:  # empty ''
             self.url = dynamic_urls[self.instrument_key]().latest_index_label_url
+            
 
     @property
     def isotimestamp(self):
@@ -88,13 +80,6 @@ class Index:
             print("Warning: Could not get the remote timestamp for update check.")
         return self._remote_timestamp
         
-    @property
-    def update_available(self) -> bool:  # Boolean indicating if there's a new index
-        "Property indicating if the index needs to be downloaded."
-        if not self.timestamp:
-            return True  # never downloaded
-        return True if self.remote_timestamp > self.timestamp else False
-
     @property
     def key_tokens(self):
         return self.key.split(".")
@@ -122,7 +107,10 @@ class Index:
 
     @property
     def label_filename(self):
-        return Path(self.url.split("/")[-1])
+        if self.url:
+            return Path(self.url.split("/")[-1])
+        else:
+            return Path.ls(self.local_dir, file_exts=[".lbl", ".LBL"])[0]
 
     @property
     def isupper(self):
@@ -134,21 +122,11 @@ class Index:
         return self.label_filename.with_suffix(new_suffix)
 
     @property
-    def label_path(self):
-        return Path(urlsplit(self.url).path)
-
-    @property
-    def table_path(self):
-        return self.label_path.with_name(self.table_filename.name)
-
-    @property
     def table_url(self):
-        tokens = urlsplit(self.url)
-        return urlunsplit(
-            tokens._replace(
-                path=str(self.label_path.with_name(self.table_filename.name))
-            )
-        )
+        if self.url:
+            return str(URL(index.url).with_suffix(".tab"))
+        else:
+            return ''
 
     @property
     def local_dir(self):
@@ -171,29 +149,6 @@ class Index:
     @property
     def parquet(self):
         return pd.read_parquet(self.local_parq_path)
-
-    def download(
-        self,
-    ):
-        """Wrapping URLs for downloading PDS indices and their label files."""
-        # check timestamp
-        label_url = self.url
-        logger.info("Downloading %s." % label_url)
-        utils.url_retrieve(label_url, self.local_label_path)
-        logger.info("Downloading %s.", self.table_url)
-        utils.url_retrieve(self.table_url, self.local_table_path)
-        print(f"Downloaded {self.local_label_path} and {self.local_table_path}")
-        if (
-            self.key == "missions.mro.hirise.indexes.edr"
-        ):  # HiRISE EDR index is broken on the PDS. Team knows.
-            print("Fixing broken EDR index...")
-            fix_hirise_edrcumindex(
-                self.local_table_path, self.local_table_path.with_name("temp.tab")
-            )
-            self.local_table_path.with_name("temp.tab").rename(self.local_table_path)
-        self.timestamp = self.remote_timestamp
-        self.update_timestamp()
-        self.convert_to_parquet()
 
     def update_timestamp(self):
         # Note: the config object writes itself out after setting any value
@@ -220,3 +175,53 @@ class Index:
 
     def __repr__(self):
         return self.__str__()
+
+# %% ../../notebooks/api/02a_pds.indexes.ipynb 7
+@patch
+def parse_key(
+    self:Index,
+    key: str,  # dotted key
+):
+    """Take care of different ways how the key could be structured.
+
+    This involves adding the sub-key `indexes` for the config file structure,
+    which is something the user of this class should not need to know.
+    """
+    tmp = key if key.startswith("missions") else "missions." + key
+    subs = tmp.split(".")
+    if subs[3] != "indexes":
+        subs.insert(3, "indexes")
+    return ".".join(subs)
+
+
+# %% ../../notebooks/api/02a_pds.indexes.ipynb 8
+@patch
+def download(self:Index):
+    """Wrapping URLs for downloading PDS indices and their label files."""
+    # check timestamp
+    label_url = self.url
+    logger.info("Downloading %s." % label_url)
+    utils.url_retrieve(label_url, self.local_label_path)
+    logger.info("Downloading %s.", self.table_url)
+    utils.url_retrieve(self.table_url, self.local_table_path)
+    print(f"Downloaded {self.local_label_path} and {self.local_table_path}")
+    if (
+        self.key == "missions.mro.hirise.indexes.edr"
+    ):  # HiRISE EDR index is broken on the PDS. Team knows.
+        print("Fixing broken EDR index...")
+        fix_hirise_edrcumindex(
+            self.local_table_path, self.local_table_path.with_name("temp.tab")
+        )
+        self.local_table_path.with_name("temp.tab").rename(self.local_table_path)
+    self.timestamp = self.remote_timestamp
+    self.update_timestamp()
+    self.convert_to_parquet()
+
+# %% ../../notebooks/api/02a_pds.indexes.ipynb 9
+@patch(as_prop=True)
+def update_available(
+        self: Index) -> bool:  # Boolean indicating if there's a new index
+    "Property indicating if the index needs to be downloaded."
+    if not self.timestamp:
+        return True  # never downloaded
+    return True if self.remote_timestamp > self.timestamp else False
