4f0f3a4debd0c84870a8ce285655a296e6f84f83
Michael Aye <kmichael.aye@gmail.com>
2023-12-19 15:59:17 +0100

    add title to CRISM notebook

--- planetarypy/pds/crism_index.py
+++ planetarypy/pds/crism_index.py
@@ -3,11 +3,11 @@
 # %% auto 0
 __all__ = ['MTRDRIndex']
 
-# %% ../../notebooks/api/02g_pds.crism_index.ipynb 1
+# %% ../../notebooks/api/02g_pds.crism_index.ipynb 2
 from yarl import URL
 from .indexes import Index
 
-# %% ../../notebooks/api/02g_pds.crism_index.ipynb 3
+# %% ../../notebooks/api/02g_pds.crism_index.ipynb 4
 class MTRDRIndex(Index):
     def __init__(self, url):
         super().__init__(key='mro.crism.mtrdr', url=url)
